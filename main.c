453a9fe43cf900a993494f0b3b649206c46cdd3e
Hardy Whitney <contact@htwhitney.com>
2022-05-16 19:53:43 -0400

    update cmakelists

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "htw_core.h"
+#include "htw_geomap.h"
+
+void testMap() {
+    Map *myMap = createMap(15, 5);
+    printMap(myMap);
+}
 
 int main(int argc, char *argv[])
 {
-    puts("Hello, World!");
+    testMap();
     return 0;
 }
