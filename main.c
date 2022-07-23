ea4301af172bad68beeb865dfebfb9aaacb871b1
Hardy Whitney <contact@htwhitney.com>
2022-07-23 13:13:38 -0400

    Add design doc to git

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include "kingdom_defs.h"
+#include "kingdom_logic.h"
+#include "kingdom_window.h"
+#include "kingdom_interaction.h"
 #include "htw_core.h"
-#include "htw_geomap.h"
+#include "htw_vulkan.h"
 
+#define LOGIC_TPS 30
+#define RENDER_TPS 60
+
+/* testing stuff, should be moved to another file
 void testMap() {
     TileMap *myTMap = createTileMap (15, 5);
     setMapTile ( myTMap, ( MapTile ){5, 0}, 14, 4);
@@ -16,52 +24,109 @@ void testMap() {
     printValueMap(myVMap);
 }
 
-Uint32 *valueMapToBitmap(ValueMap *map, Uint32 format) {
+// If bmp is NULL, a new bitmap of appropriate size is allocated. Retuns a pointer to bmp.
+Uint32 *valueMapToBitmap(ValueMap *map, Uint32 *bmp, Uint32 format) {
     int mapSize = map->width * map->height;
-    Uint32 *bmp = malloc(mapSize * sizeof(Uint32));
+    if (bmp == NULL)
+        bmp = malloc(mapSize * sizeof(Uint32));
     for (int i = 0; i < mapSize; i++) {
         int value = map->values[i];
-        value = remap_int(0, map->maxValue, 0, 255, value);
+        //value = remap_int(0, map->maxValue, 0, 255, value);
         SDL_PixelFormat *pixelFormat = SDL_AllocFormat(format);
         bmp[i] = SDL_MapRGB(pixelFormat, value, 0, 0);
     }
     return bmp;
+}
+*/
+
+typedef struct {
+    kd_LogicInputState *input;
+    kd_WorldState *world;
+    Uint32 interval;
+    KD_APPSTATE *volatile appState;
+} LogicLoopInput;
+
+// started as a seperate thread by SDL
+int logicLoop(void *ptr) { // TODO: can this be a specific input type? If so, change to LogicLoopInput*
+    // Extract input data
+    LogicLoopInput *loopInput = (LogicLoopInput*)ptr;
+    kd_LogicInputState *input = loopInput->input;
+    kd_WorldState *world = loopInput->world;
+    Uint32 interval = loopInput->interval;
+    KD_APPSTATE *appState = loopInput->appState;
+
+    while (*appState == KD_APPSTATE_RUNNING) {
+        Uint64 startTime = SDL_GetTicks64();
+
+        simulateWorld(input, world);
+
+        // delay until end of frame
+        Uint64 endTime = SDL_GetTicks64();
+        Uint64 duration = endTime - startTime;
+        if (duration < interval) {
+            SDL_Delay(interval - duration);
+        }
+    }
+    return 0;
+}
+
+// must run on the main thread
+int interactiveWindowLoop (kd_UiState *ui, kd_LogicInputState *logicInput, kd_WorldState *world, Uint32 interval, KD_APPSTATE *volatile appState) {
+    /* for testing SDL direct draw
+    Uint32 terrainFormat = SDL_PIXELFORMAT_RGBA32;
+    Uint32 *bmp = valueMapToBitmap(world->heightMap, NULL, terrainFormat);
+    SDL_Surface *terrain = NULL;
+    int width = world->heightMap->width;
+    int height = world->heightMap->height;
+    terrain = SDL_CreateRGBSurfaceWithFormatFrom(bmp, width, height, sizeof(Uint32) / 8, width * sizeof(Uint32), terrainFormat);
+    */
+
+    htw_vkContext *vkContext = htw_createVkContext();
+
+    while (*appState == KD_APPSTATE_RUNNING) {
+        Uint64 startTime = SDL_GetTicks64();
+
+        handleInputs(ui, logicInput, appState);
+
+        kd_renderFrame(vkContext, ui, world);
+
+        // delay until end of frame
+        Uint64 endTime = SDL_GetTicks64();
+        Uint64 duration = endTime - startTime;
+        if (duration < interval) {
+            SDL_Delay(interval - duration);
+        }
+    }
+
+    htw_destroyVkContext(vkContext);
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     //loadTileDefinitions ("resources/cell_types");
 
-    int running = 1;
-    SDL_Window *window = NULL;
-    SDL_Surface *surface = NULL;
-    SDL_Event e;
+    // Must incidate where used that the value of this is volatile, as it may get updated by another thread
+    KD_APPSTATE appState = KD_APPSTATE_RUNNING;
 
+    printf("Initizlizing SDL...\n");
     SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow("kingdom", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    surface = SDL_GetWindowSurface(window);
+    kd_UiState *ui = createUiState();
+    kd_WorldState *world = createWorldState("Test World");
+    initializeWorldState(world);
+    kd_LogicInputState *logicInput = createLogicInputState();
 
-    // Paint some terrain
-    ValueMap *myVMap = createValueMap(20, 10, 255);
-    fillGradient(myVMap, 0, 255);
-    Uint32 terrainFormat = SDL_PIXELFORMAT_RGBA32;
-    //Uint32 *bmp = valueMapToBitmap(myVMap, terrainFormat);
-    SDL_Surface *terrain = NULL;
-    //terrain = SDL_CreateRGBSurfaceWithFormatFrom(bmp, myVMap->width, myVMap->height, sizeof(Uint32) / 8, myVMap->width * sizeof(Uint32), terrainFormat);
+    Uint32 logicInterval = 1000 / LOGIC_TPS;
+    Uint32 frameInterval = 1000 / RENDER_TPS;
 
-    while (running) {
-        while (SDL_PollEvent(&e) != 0) {
-            if (e.type == SDL_QUIT)
-                running = 0;
-        }
+    LogicLoopInput logicLoopParams = {logicInput, world, logicInterval, &appState};
+    SDL_Thread *logicThread = SDL_CreateThread(logicLoop, "logic", &logicLoopParams);
+    int logicThreadResult;
 
-        //SDL_BlitSurface(terrain, NULL, surface, NULL);
+    interactiveWindowLoop(ui, logicInput, world, frameInterval, &appState);
 
-        SDL_UpdateWindowSurface(window);
-    }
-
-    SDL_DestroyWindow(window);
+    SDL_WaitThread(logicThread, &logicThreadResult);
 
     SDL_Quit();
 
